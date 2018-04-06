00c26d617f5a8fae0d32c3a5906d22cf2301490b
nmash1937 <35733499+nmash1937@users.noreply.github.com>
2018-04-06 11:37:11 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,84 +1,101 @@
+
 import java.util.Scanner;
 import java.util.Stack;
 /**
- * Calculator: solves infix notated expression involving +-/*()
+ * 
  * @author Nick Masciandaro
- *CSCI 258
+ *	Written for Data Structures then extended upon for multi digit numbers
  */
 public class Calculator {
-	/**
-	 * Runs program
-	 * @param args
-	 */
 	public static void main(String[] args){
-		System.out.println("Type Mathematical Expression:");
+		System.out.println("Type Mathematical equation:");
 		Scanner in = new Scanner(System.in);
 		String input = in.nextLine();
-		String[] split = input.split(" ");
-		input = "";
-		for(String a : split){
-			input += a;
-		}
 		String postfix = infixToPostfix(input);
 		System.out.println(postfix);
-		Double answer = solve(postfix);
+		int answer = solve(postfix);
 		System.out.println(answer);
 		
 	}
 	
-	/**
-	 * Solves the postfix equation into an integer
-	 * @param postfix expression yet to be solved
-	 * @return answer of mathematic postfix expression
-	 */
-	public static Double solve(String postfix){
-		double ans = 0;
-		Stack<Double> stack = new Stack<Double>();
+	public static int solve(String postfix){
+		int ans = 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		Stack<Integer> number = new Stack<Integer>();
+		boolean wasDigit = false;
+		int num = 0;
+		int count = 0;
+		int power = 0;
 		for(char c : postfix.toCharArray()){
-			//checks if digit
-			if(isDigit(c)){
-				stack.push( Double.parseDouble("" +c));
+			//checks if last char was a digit and current one is not
+			if(wasDigit && !isDigit(c)) {
+				int a = 0;
+				power = count;
+				while(power!=0) {
+					a += (int) (number.pop()*Math.pow(10, count-power));
+					power--;
+				}
+				stack.push(Integer.parseInt("" +a));
+				num = 0;
+				count = 0;
+				wasDigit = false;
 			}
-			else{
-				Double b = stack.pop();
-				Double a = stack.pop();
+			//Checks if Digit
+			if(isDigit(c)){
+				number.push(Integer.parseInt("" +c));
+				count++;
+				wasDigit = true;
+				
+			}
+			else if(isOp(c)){
+				wasDigit = false;
+				int b = stack.pop();
+				int a = stack.pop();
 				if(c == '*'){
-					double res = a*b;
+					int res = a*b;
 					stack.push(res);
 				}
 				else if(c=='/'){
-					double res = a/b;
+					int res = a/b;
 					stack.push(res);
 				}
 				else if(c == '-'){
-					double res = a-b;
+					int res = a-b;
 					stack.push(res);
 				}
 				else if(c =='+'){
-					double res = a+b;
+					int res = a+b;
 					stack.push(res);
+					System.out.println(res);
 				}
+				wasDigit = false;
 			}
+			
 		}
 		return stack.pop();
 	}
 	
-	/**
-	 * Converts a infix equation into a postfix equation
-	 * @param infix original mathematical expression
-	 * @return converted postfix mathematical expression
-	 */
 	public static String infixToPostfix(String infix){
 		Stack<Character> stack = new Stack<Character>();
 		String postfix = "";
 		boolean out = true;
+		boolean wasDigit = false;
+		String num = "";
+		
 		for(char c : infix.toCharArray()){
+			if(wasDigit && !isDigit(c)){		
+						postfix+=' ';
+						postfix+=num;
+						num = "";	
+			}
 			//Checks if digit
 			if(isDigit(c)){
-				postfix+=c;
+				num+=c;
+				wasDigit = true;
 			}
 			//Checks if )
 			else if(c == ')'){
+				wasDigit=false;
 				while(!stack.isEmpty()&& out){
 					if(!(stack.peek() == '(')){
 						postfix += stack.pop();
@@ -90,28 +107,32 @@ public class Calculator {
 				out=true;
 			}
 			//Checks if (
-			else if(c == '(') stack.push(c);
+			else if(c == '(') {
+				stack.push(c);
+				wasDigit=false;
+			}
 			//left with operator
 			else{
+				wasDigit=false;
 				while(!stack.empty() && checkPriority(c, stack.peek()) && !(stack.peek()=='(')  ){
+					postfix+= ' ';
 					postfix+=stack.pop();
 				}
 				stack.push(c);
 			}
 		}
+		if(num!="") {
+			postfix+=" ";
+			postfix+=num;
+		}
 		while(!stack.isEmpty()){
+			postfix+= ' ';
 			postfix+=stack.pop();
 		}
 		return postfix;
 		
 	}
 	
-	/**
-	 * Determines if first operator is higher or lower than the second operator in terms of priority
-	 * @param c next operator
-	 * @param d: top of stack operator
-	 * @return if operator d is lower than c
-	 */
 	public static boolean checkPriority(char c, char d){
 		int rankC = 0;
 		int rankD = 0;
@@ -126,15 +147,15 @@ public class Calculator {
 		
 	}
 	
-	/**
-	 * Determines if the character given is a digit
-	 * @param c character to be examined
-	 * @return true if digit, false if not
-	 */
 	public static boolean isDigit(char c){
 		if(c == '0' || c == '1' ||c == '2' ||c == '3' ||
 				c == '4' ||c == '5' ||c == '6' ||c == '7' ||
 				c == '8' ||c == '9') return true;
+		return false;
+	}
+	
+	public static boolean isOp(char c) {
+		if(c=='*'||c=='/'||c=='-'||c=='+')return true;
 		return false;
 	}
 }
